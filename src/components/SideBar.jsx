fe065c5c8243922193a9970117beaddadcc37eb9
Vaibhav Malviya <001codemaster@gmail.com>
2026-02-10 21:12:23 +0530

    fixed side bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -18,29 +18,39 @@ function SideBar({ setShow }) {
   };
 
   return (
-    <>
-      <div data-aos="fade-left" className='w-[80%] h-[100%] lg:hidden bg-blue-100 z-50 absolute top-0 right-0 shadow-lg shadow-black rounded-b-lg'>
-        <button onClick={handleClose} className='px-6 py-5 text-red-500 font-extrabold text-2xl'>X</button>
-        <ul className='list-none flex flex-col gap-5 justify-center px-6 mt-[20%] font-bold'>
-          <li className='flex flex-row items-center gap-3' onClick={() => handleNavigate('/')}>
-            <FaHome /><a href="#">Home</a>
-          </li>
-          <li className='flex flex-row items-center gap-3' onClick={() => handleNavigate('/aboutus')}>
-            <MdPeopleAlt /><a href="#">About Us</a>
-          </li>
-          
-          <li className='flex flex-row items-center gap-3' onClick={() => handleNavigate('/FormSubmission')}>
-            <FaFileWaveform /><a href="#">Form Submission</a>
-          </li>
-          <li className='flex flex-row items-center gap-3' onClick={() => handleNavigate('/FDInfo')}>
-            <FaFileWaveform /><a href="#">Fixed Deposit</a>
-          </li>
-          <li className='flex flex-row items-center gap-3' onClick={() => handleNavigate('/contactus')}>
-            <MdOutlinePermContactCalendar /><a href="#">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div
+      data-aos="fade-left"
+      className="w-[80%] h-screen lg:hidden bg-blue-100 z-50 fixed top-0 right-0 shadow-lg shadow-black rounded-b-lg"
+    >
+      <button
+        onClick={handleClose}
+        className="px-6 py-5 text-red-500 font-extrabold text-2xl"
+      >
+        X
+      </button>
+
+      <ul className="list-none flex flex-col gap-5 justify-center px-6 mt-[20%] font-bold">
+        <li className="flex items-center gap-3" onClick={() => handleNavigate('/')}>
+          <FaHome /> Home
+        </li>
+
+        <li className="flex items-center gap-3" onClick={() => handleNavigate('/aboutus')}>
+          <MdPeopleAlt /> About Us
+        </li>
+
+        <li className="flex items-center gap-3" onClick={() => handleNavigate('/FormSubmission')}>
+          <FaFileWaveform /> Form Submission
+        </li>
+
+        <li className="flex items-center gap-3" onClick={() => handleNavigate('/FDInfo')}>
+          <FaFileWaveform /> Fixed Deposit
+        </li>
+
+        <li className="flex items-center gap-3" onClick={() => handleNavigate('/contactus')}>
+          <MdOutlinePermContactCalendar /> Contact Us
+        </li>
+      </ul>
+    </div>
   );
 }
 
